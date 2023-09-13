5c2b03ee013f75f8b318d70c76ea354b21ddb6b8
DarkLuminous <144906696+DarkLuminous@users.noreply.github.com>
2023-09-13 19:31:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Complete Name: Roger E. Bao Jr.")
+print("Course: BSCS")
+print("Section: CS1E")
+print("Age: Classified")
+print("Birthday: mm/dd/yyyy ")
+print("Complete Home Address: Barra opol Mis. Or. Blk 24 Lot 13")
